19b6924bef7c00c4f04d525e8b33a7527081159f
IlorDash <ilordash@mail.ru>
2022-09-25 02:26:48 +0300

    Update alu_testbench.sv

--- Lab_1.srcs/sim_1/new/alu_testbench.sv
+++ Lab_1.srcs/sim_1/new/alu_testbench.sv
@@ -1,27 +1,7 @@
 `timescale 1ns / 1ps
-
-`define ADD 5'b00000
-`define SUB 5'b01000
-`define SLL 5'b00001
-`define SLT 5'b00010
-`define SLTU 5'b00011
-`define XOR 5'b00100
-`define SRL 5'b00101
-`define SRA 5'b01101
-`define OR 5'b00110
-`define AND 5'b00111
-`define BEQ 5'b11000
-`define BNE 5'b11001
-`define BLT 5'b11100
-`define BGE 5'b11101
-`define BLTU 5'b11110
-`define BGEU 5'b11111
-
-`define CMD_NUM 16
+`include "defines.v"
 
 // Max tests num = 999
-`define TESTS_ITER 10
-`define WORD_LEN 32
 
 module alu_testbench ();
 
@@ -31,7 +11,7 @@ module alu_testbench ();
   wire                 flag;
   wire [`WORD_LEN-1:0] res;
 
-  alu_riscv dut (
+  my_alu dut (
       .A(A),
       .B(B),
       .ALUOp(cmd),
