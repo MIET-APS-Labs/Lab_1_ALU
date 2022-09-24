3c1f4a540fe233ff3d9c6a1c6da78c3ceee907aa
IlorDash <ilordash@mail.ru>
2022-09-25 02:26:50 +0300

    Update adder_testbench.sv

--- Lab_1.srcs/sim_1/new/adder_testbench.sv
+++ Lab_1.srcs/sim_1/new/adder_testbench.sv
@@ -6,19 +6,23 @@
 
 module adder_testbench ();
 
-  reg [`WORD_LEN-1:0] A, B, B_sub;
+  reg [`WORD_LEN-1:0] A, B;
+  reg sub;
 
   wire carry;
   wire [`WORD_LEN-1:0] res;
   N_bit_full_adder dut (
       .num1(A),
       .num2(B),
+      .sub(sub),
+
       .carry_out(carry),
       .result(res)
   );
 
   initial begin
     $display("Testing SUM");
+    sub = 0;
     for (int i = 0; i < `TESTS_NUM; i++) begin
       A = 1000 - i * (2 + i % 2);
       B = i + 100 + i % 2;
@@ -31,15 +35,15 @@ module adder_testbench ();
     end
 
     $display("Testing SUB");
+    sub = 1;
     for (int i = 0; i < `TESTS_NUM; i++) begin
       A = 1000 - i * (2 + i % 2);
-      B_sub = i + 100 + i % 2;
-      B = (~B_sub + 1);
+      B = i + 100 + i % 2;
       #100;
-      if (res == (A - B_sub)) begin
-        $display("%d) GOOD %d - %d = %d", i, A, B_sub, res);
+      if (res == (A - B)) begin
+        $display("%d) GOOD %d - %d = %d", i, A, B, res);
       end else begin
-        $display("%d) BAD %d - %d = %d", i, A, B_sub, res);
+        $display("%d) BAD %d - %d = %d", i, A, B, res);
       end
     end
 
