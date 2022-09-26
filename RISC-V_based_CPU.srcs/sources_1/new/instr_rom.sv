d0dac89bdd86522415f2584849a2adfc5bc66cf1
IlorDash <ilordash@mail.ru>
2022-09-27 00:34:18 +0300

    creatred ROM for instructions

---
+++ RISC-V_based_CPU.srcs/sources_1/new/instr_rom.sv
@@ -0,0 +1,15 @@
+`timescale 1ns / 1ps
+
+module instr_rom #(
+    parameter WIDTH = 32,
+    parameter DEPTH = 64
+) (
+    input logic [$clog2(DEPTH)-1:0] addr,
+    output logic [WIDTH-1:0] rd
+);
+
+  initial $readmemb("prog.txt", ROM, 0, DEPTH - 1);
+
+  logic [WIDTH-1:0] ROM[0:DEPTH-1];
+  assign rd = ROM[addr];
+endmodule
