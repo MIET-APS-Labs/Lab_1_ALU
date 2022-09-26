fb61398f90bddb205f5f5176e5f490fddd82b49c
IlorDash <ilordash@mail.ru>
2022-09-27 00:34:49 +0300

    Create N_bit_full_adder.sv

---
+++ RISC-V_based_CPU.srcs/sources_1/new/N_bit_full_adder.sv
@@ -0,0 +1,43 @@
+`timescale 1ns / 1ps
+
+
+module N_bit_full_adder #(
+    parameter DIGIT = 32
+) (
+    input [DIGIT-1:0] num1,
+    input [DIGIT-1:0] num2,
+    input sub,
+
+    output wire carry_out,
+    output reg [DIGIT-1:0] result
+);
+
+  wire [DIGIT-1:0] num2_real = sub ? ~num2 + 1 : num2;
+
+  wire [DIGIT-1:0] carry;
+  genvar i;
+  generate
+    full_adder f (
+        num1[0],
+        num2_real[0],
+        0,
+        carry[0],
+        result[0]
+    );
+
+    for (i = 1; i < DIGIT; i = i + 1) begin
+      full_adder f (
+          num1[i],
+          num2_real[i],
+          carry[i-1],
+          carry[i],
+          result[i]
+      );
+    end
+    assign carry_out = carry[DIGIT-1];
+  endgenerate
+
+
+
+
+endmodule
