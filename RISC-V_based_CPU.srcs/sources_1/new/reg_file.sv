48bbc233d066c59332f15720381424b138f21832
IlorDash <ilordash@mail.ru>
2022-10-02 19:20:32 +0300

    added returning 0 when read addres 2 = 0

--- RISC-V_based_CPU.srcs/sources_1/new/reg_file.sv
+++ RISC-V_based_CPU.srcs/sources_1/new/reg_file.sv
@@ -5,10 +5,10 @@ module reg_file #(
     parameter WIDTH  = 32
 ) (
     input clk,
-    input [$clog2(WIDTH)-1:0] adr1,  // address
-    input [$clog2(WIDTH)-1:0] adr2,  // address
-    input [$clog2(WIDTH)-1:0] adr3,  // address
-    input [WORD_LEN:0] wd,  // Write Data
+    input [$clog2(WIDTH)-1:0] adr1,  // read address 1
+    input [$clog2(WIDTH)-1:0] adr2,  // read address 2
+    input [$clog2(WIDTH)-1:0] adr3,  // write address
+    input [WORD_LEN:0] wd3,  // Write Data
     input we3,  // Write Enable
 
     output [WORD_LEN:0] rd1,  // Read Data
@@ -17,11 +17,11 @@ module reg_file #(
   logic [WORD_LEN-1:0] RAM[0:WIDTH-1];
 
   assign rd1 = (adr1 == 0) ? 0 : RAM[adr1];
-  assign rd2 = RAM[adr2];
+  assign rd2 = (adr2 == 0) ? 0 : RAM[adr2];
 
   always_ff @(posedge clk) begin
     if (we3 && adr3) begin
-      RAM[adr3] <= wd;
+      RAM[adr3] <= wd3;
     end
   end
 
