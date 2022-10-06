a45af8ecedafbcefcb5a3ae9bfb0052ceb352f6e
IlorDash <ilordash@mail.ru>
2022-10-06 18:32:30 +0300

    deleted logic in inputs/outputs

--- RISC-V_based_CPU.srcs/sources_1/new/full_adder.sv
+++ RISC-V_based_CPU.srcs/sources_1/new/full_adder.sv
@@ -1,14 +1,13 @@
 `timescale 1ns / 1ps
 
 module full_adder (
-    input logic A,
-    input logic B,
-    input logic carry_in,
+    input A,
+    input B,
+    input carry_in,
 
-    output logic carry_out,
-    output logic res
+    output carry_out,
+    output res
 );
-
 
   assign res = A ^ B ^ carry_in;
   assign carry_out = (A & B) | (A & carry_in) | (B & carry_in);
