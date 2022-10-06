fc84bd9571e3f42476cd8048d5bfffd895f0c2be
IlorDash <ilordash@mail.ru>
2022-10-06 18:34:04 +0300

    deleted temp opcodes

--- RISC-V_based_CPU.srcs/sources_1/new/myCPU_params.v
+++ RISC-V_based_CPU.srcs/sources_1/new/myCPU_params.v
@@ -3,51 +3,30 @@
 //////////////////////////////////////////////
 //  ALU Defines
 
-`define ALU_OP_LEN 4
+`define ALU_OP_LEN 5
 `define ALU_OP_NUM 16
 
-// //  ALU op codes
-// `define ADD 5'b00000
-// `define SUB 5'b01000
-// `define SLL 5'b00001
-// `define SLT 5'b00010
-// `define SLTU 5'b00011
-// `define XOR 5'b00100
-// `define SRL 5'b00101
-// `define SRA 5'b01101
-// `define OR 5'b00110
-// `define AND 5'b00111
-// `define BEQ 5'b11000
-// `define BNE 5'b11001
-// `define BLT 5'b11100
-// `define BGE 5'b11101
-// `define BLTU 5'b11110
-// `define BGEU 5'b11111
+//  ALU op codes
+`define ADD 5'b00000
+`define SUB 5'b01000
+`define SLL 5'b00001
+`define SLT 5'b00010
+`define SLTU 5'b00011
+`define XOR 5'b00100
+`define SRL 5'b00101
+`define SRA 5'b01101
+`define OR 5'b00110
+`define AND 5'b00111
+`define BEQ 5'b11000
+`define BNE 5'b11001
+`define BLT 5'b11100
+`define BGE 5'b11101
+`define BLTU 5'b11110
+`define BGEU 5'b11111
 
-// //  SIMD op codes
-// `define SIMD_ADD 5'b01001	
-// `define SIMD_SUB 5'b01010	
-
-
-/////////////////////////////////////////////
-//TEMP ALU op codes
-`define ADD 4'b0000
-`define SUB 4'b0001
-`define SLL 4'b0010
-`define SLT 4'b0011
-`define SLTU 4'b0100
-`define XOR 4'b0101
-`define SRL 4'b0110
-`define SRA 4'b0111
-`define OR 4'b1000
-`define AND 4'b1001
-`define BEQ 4'b1100
-`define BNE 4'b1010
-`define BLT 4'b1011
-`define BGE 4'b1101
-`define BLTU 4'b1110
-`define BGEU 4'b1111
-/////////////////////////////////////////////
+//  SIMD op codes
+`define SIMD_ADD 5'b01001	
+`define SIMD_SUB 5'b01010	
 
 
 //  SIMD Rules
