9bd76e8c81c336d1af52f703e5f5e5258ff1a212
IlorDash <ilordash@mail.ru>
2022-10-11 01:29:48 +0300

    renamed instruction format defines from Lab_2 to C_COBRA_INSTR_...

--- RISC-V_based_CPU.srcs/sources_1/new/RISC_V_based_CPU_top.sv
+++ RISC-V_based_CPU.srcs/sources_1/new/RISC_V_based_CPU_top.sv
@@ -30,7 +30,7 @@ module RISC_V_based_CPU_top (
   logic [`WORD_LEN-1:0] reg_write_data;
 
   always_comb begin
-    case (instruction[`INSTR_WS])
+    case (instruction[`C_COBRA_INSTR_WS])
       2'b01: begin
         reg_write_data = sw_val_ext;
       end
@@ -51,11 +51,11 @@ module RISC_V_based_CPU_top (
   `RF_WIDTH
   ) my_reg_file (
       .clk (CLK100MHZ),
-      .adr1(instruction[`INSTR_RA1]),
-      .adr2(instruction[`INSTR_RA2]),
-      .adr3(instruction[`INSTR_WA]),
+      .adr1(instruction[`C_COBRA_INSTR_RA1]),
+      .adr2(instruction[`C_COBRA_INSTR_RA2]),
+      .adr3(instruction[`C_COBRA_INSTR_WA]),
       .wd3 (reg_write_data),
-      .we3 (instruction[`INSTR_WS_1] | instruction[`INSTR_WS_2]),
+      .we3 (instruction[`C_COBRA_INSTR_WS_1] | instruction[`C_COBRA_INSTR_WS_2]),
 
       .rd1(reg_read_data1),
       .rd2(reg_read_data2)
@@ -65,7 +65,7 @@ module RISC_V_based_CPU_top (
 
   logic [`WORD_LEN-1:0] const_val_ext;
   assign const_val_ext = {
-    {(`WORD_LEN - `CONST_LEN) {instruction[`INSTR_CONST+(`CONST_LEN-1)]}}, instruction[`INSTR_CONST]
+    {(`WORD_LEN - `CONST_LEN) {instruction[`C_COBRA_INSTR_CONST+(`CONST_LEN-1)]}}, instruction[`C_COBRA_INSTR_CONST]
   };
 
 
@@ -80,7 +80,7 @@ module RISC_V_based_CPU_top (
       PC <= 0;
     end else begin
       //$display("SW: %b\nReset: %b\nProgram counter: %d", SW, rst, PC);
-      if ((instruction[`INSTR_C] & ALU_flag) | instruction[`INSTR_B]) begin
+      if ((instruction[`C_COBRA_INSTR_C] & ALU_flag) | instruction[`C_COBRA_INSTR_B]) begin
         PC <= PC + const_val_ext;
       end else begin
         PC <= PC + 1;
@@ -90,14 +90,13 @@ module RISC_V_based_CPU_top (
     // B[31] C[30] WS[29:28] ALUop[27:23] RA1[22:18] RA2[17:13] CONST[12:5] WA[4:0]
     // $display(
     //     "PC = %d; B = %b; C = %b; WS = %b; ALUop = %b; RA1 = %b; RA2 = %b; CONST = %b; WA = %b",
-    //     PC, instruction[`INSTR_B], instruction[`INSTR_C], instruction[`INSTR_WS],
-    //     instruction[`INSTR_ALUop], instruction[`INSTR_RA1], instruction[`INSTR_RA2],
-    //     instruction[`INSTR_CONST], instruction[`INSTR_WA]);
+    //     PC, instruction[`C_COBRA_INSTR_B], instruction[`C_COBRA_INSTR_C], instruction[`C_COBRA_INSTR_WS],
+    //     instruction[`C_COBRA_INSTR_ALUop], instruction[`C_COBRA_INSTR_RA1], instruction[`C_COBRA_INSTR_RA2],
+    //     instruction[`C_COBRA_INSTR_CONST], instruction[`C_COBRA_INSTR_WA]);
 
   end
 
-  assign LED[5:0]  = PC;
-  assign LED[15:6] = reg_read_data1[9:0];
+
 
 
   // Switches value sign extender
@@ -116,11 +115,18 @@ module RISC_V_based_CPU_top (
   alu #(`WORD_LEN, `ALU_OP_LEN) my_alu (
       .A(reg_read_data1),
       .B(reg_read_data2),
-      .ALUOp(instruction[`INSTR_ALUop]),
+      .ALUOp(instruction[`C_COBRA_INSTR_ALUop]),
 
       .Flag  (ALU_flag),
       .Result(ALU_res)
   );
+
+
+
+
+  assign LED[5:0]  = PC;
+  assign LED[15:6] = reg_read_data1[9:0]; 
+
 
   disp_HEX my_disp_HEX (
       .CLK(CLK100MHZ),
