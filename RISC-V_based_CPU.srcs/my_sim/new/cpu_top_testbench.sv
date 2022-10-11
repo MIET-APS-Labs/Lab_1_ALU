e3581e87195e79bf2a1d2d103bbd041ba4d95983
IlorDash <ilordash@mail.ru>
2022-10-11 18:18:56 +0300

    added testing reset

--- RISC-V_based_CPU.srcs/my_sim/new/cpu_top_testbench.sv
+++ RISC-V_based_CPU.srcs/my_sim/new/cpu_top_testbench.sv
@@ -1,12 +1,15 @@
 `timescale 1ns / 1ps
 
-`define SWITCHES_NUM 16'b0000000000000101   // = 2
+`define SWITCHES_NON_RST 16'b1000000000000010   // = 2
+`define SWITCHES_RST 16'b0000000000000000   // = 0
 
 module cpu_top_testbench ();
 
   logic [6:0] hex;
   logic [7:0] dig;
   logic [15:0] leds;
+
+  logic [15:0] sw;
 
   logic CLK;
   parameter PERIOD = 20;
@@ -16,9 +19,16 @@ module cpu_top_testbench ();
     #(PERIOD / 2);
   end
 
+  always begin
+    sw = `SWITCHES_NON_RST;
+    #100000;
+    sw = `SWITCHES_RST;
+    #100000;
+  end
+
   RISC_V_based_CPU_top dut (
       .CLK100MHZ(CLK),
-      .SW(`SWITCHES_NUM),
+      .SW(sw),
 
       .C  (hex),
       .AN (dig),
