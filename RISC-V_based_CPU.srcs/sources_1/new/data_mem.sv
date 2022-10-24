d2f28fad194d642509dbd45f06bab30be33b632c
IlorDash <ilordash@mail.ru>
2022-10-24 15:10:15 +0300

    created simple 32 bit addressable data mem

--- RISC-V_based_CPU.srcs/sources_1/new/data_mem.sv
+++ RISC-V_based_CPU.srcs/sources_1/new/data_mem.sv
@@ -15,7 +15,7 @@ module data_mem #(
     parameter WIDTH = 256
 ) (
     input clk,
-    input [WORD_LEN-1:0] adr,  // read/write address
+    input [$clog2(WIDTH)-1:0] adr,  // read/write address
     input [WORD_LEN-1:0] wd,  // Write Data
     input we,  // Write Enable
     input [MEM_SIZE_OPT_BIT-1:0] size,  // Write Enable
@@ -25,60 +25,70 @@ module data_mem #(
 );
   logic [WORD_LEN-1:0] RAM[0:WIDTH-1];
 
-  always_comb begin
-    case (size)
-      `LDST_B: begin
-        rd <= {{(`WORD_LEN - `BYTE_LEN) {0}}, RAM[adr[$clog2(WIDTH)+1:2]][`BYTE_LEN-1:0]};
-      end
-
-      `LDST_H: begin
-        rd <= {{(`WORD_LEN / 2) {0}}, RAM[adr[$clog2(WIDTH)+1:2]][`WORD_LEN/2-1:0]};
-      end
-
-      `LDST_W: begin
-        rd <= RAM[adr[$clog2(WIDTH)+1:2]];
-      end
-
-      `LDST_BU: begin
-        rd <= {{(`WORD_LEN - `BYTE_LEN) {0}}, RAM[adr[$clog2(WIDTH)+1:2]][`BYTE_LEN-1:0]};
-      end
-
-      `LDST_HU: begin
-        rd <= {{(`WORD_LEN / 2) {0}}, RAM[adr[$clog2(WIDTH)+1:2]][`WORD_LEN/2-1:0]};
-      end
-      default: begin
-        rd <= `WORD_LEN'b0;
-      end
-    endcase
-  end
-
+  assign rd = RAM[adr];
 
   always_ff @(posedge clk) begin
     if (we) begin
-      case (size)
-        `LDST_B: begin
-          RAM[adr[$clog2(WIDTH)+1:2]][`BYTE_LEN-1:0] <= wd[`BYTE_LEN-1:0];
-        end
-
-        `LDST_H: begin
-          RAM[adr[$clog2(WIDTH)+1:2]][`WORD_LEN/2-1:0] <= wd[`WORD_LEN/2-1:0];
-        end
-
-        `LDST_W: begin
-          RAM[adr[$clog2(WIDTH)+1:2]] <= wd;
-        end
-
-        `LDST_BU: begin
-          RAM[adr[$clog2(WIDTH)+1:2]][`BYTE_LEN-1:0] <= wd[`BYTE_LEN-1:0];
-        end
-
-        `LDST_HU: begin
-          RAM[adr[$clog2(WIDTH)+1:2]][`WORD_LEN/2-1:0] <= wd[`WORD_LEN/2-1:0];
-        end
-        default: begin
-        end
-      endcase
-      //$display("\nDATA_MEM In write case: adr = %b; wd = %b; RAM[adr3] = %b\n", adr[$clog2(WIDTH)+1:2], wd, adr[$clog2(WIDTH)+1:2]);
+      RAM[adr] <= wd;
+      //$display("\nREG_FILE In write case: adr3 = %d; wd3 = %b; RAM[adr3] = %b\n", adr3, wd3, RAM[adr3]);
     end
   end
+
+  // always_comb begin
+  //   case (size)
+  //     `LDST_B: begin
+  //       rd <= {{(`WORD_LEN - `BYTE_LEN) {0}}, RAM[adr][`BYTE_LEN-1:0]};
+  //     end
+
+  //     `LDST_H: begin
+  //       rd <= {{(`WORD_LEN / 2) {0}}, RAM[adr][`WORD_LEN/2-1:0]};
+  //     end
+
+  //     `LDST_W: begin
+  //       rd <= RAM[adr];
+  //     end
+
+  //     `LDST_BU: begin
+  //       rd <= {{(`WORD_LEN - `BYTE_LEN) {0}}, RAM[adr][`BYTE_LEN-1:0]};
+  //     end
+
+  //     `LDST_HU: begin
+  //       rd <= {{(`WORD_LEN / 2) {0}}, RAM[adr][`WORD_LEN/2-1:0]};
+  //     end
+  //     default: begin
+  //       rd <= `WORD_LEN'b0;
+  //     end
+  //   endcase
+  // end
+
+
+  // always_ff @(posedge clk) begin
+  //   if (we) begin
+  //     case (size)
+  //       `LDST_B: begin
+  //         RAM[adr][`BYTE_LEN-1:0] <= wd[`BYTE_LEN-1:0];
+  //       end
+
+  //       `LDST_H: begin
+  //         RAM[adr][`WORD_LEN/2-1:0] <= wd[`WORD_LEN/2-1:0];
+  //       end
+
+  //       `LDST_W: begin
+  //         RAM[adr] <= wd;
+  //       end
+
+  //       `LDST_BU: begin
+  //         RAM[adr][`BYTE_LEN-1:0] <= wd[`BYTE_LEN-1:0];
+  //       end
+
+  //       `LDST_HU: begin
+  //         RAM[adr][`WORD_LEN/2-1:0] <= wd[`WORD_LEN/2-1:0];
+  //       end
+  //       default: begin
+  //       end
+  //     endcase
+  //     //$display("\nDATA_MEM In write case: adr = %b; wd = %b; RAM[adr] = %b\n", adr, wd, RAM[adr]);
+  //   end
+  // end
+
 endmodule
