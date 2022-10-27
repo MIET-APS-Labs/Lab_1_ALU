9566b1105a0df82f6b189f1e16d9bc9e90351a5c
IlorDash <ilordash@mail.ru>
2022-10-28 00:08:29 +0300

    testing nullifying if INIT_FILE not specified

--- RISC-V_based_CPU.srcs/my_sim/new/instr_rom_testbench.sv
+++ RISC-V_based_CPU.srcs/my_sim/new/instr_rom_testbench.sv
@@ -15,21 +15,38 @@ module instr_rom_testbench ();
     #(PERIOD / 2);
   end
 
-  logic [`WORD_LEN-1:0] instruction;
-  instr_rom dut (
+  logic [`WORD_LEN-1:0] instruction_init;
+  instr_rom #(`INSTR_WIDTH, `INSTR_DEPTH, "prog.txt") dut_init (
       .addr(clk_cntr),
-      .rd  (instruction)
+      .rd  (instruction_init)
+  );
+
+  logic [`WORD_LEN-1:0] instruction_nullifyed;
+  instr_rom #(`INSTR_WIDTH, `INSTR_DEPTH) dut_nullifyed (
+      .addr(clk_cntr),
+      .rd  (instruction_nullifyed)
   );
 
   parameter COUNTER_WIDTH = $clog2(`INSTR_DEPTH);
   bit [COUNTER_WIDTH-1:0] clk_cntr;
   initial begin
+    $display("\nCheck Instr ROM initialized with mem init file\n");
     for (int i = 0; i < `INSTR_DEPTH; i++) begin
       @(posedge CLK);
       #5;
-      $display("%d) Instruction = %b", clk_cntr, instruction);
+      $display("%d) Instruction = %b", clk_cntr, instruction_init);
       clk_cntr <= clk_cntr + 1;
     end
-      $finish;
+
+    $display("\nCheck nullifyed Instr ROM\n");
+    clk_cntr <= 0;
+    for (int i = 0; i < `INSTR_DEPTH; i++) begin
+      @(posedge CLK);
+      #5;
+      $display("%d) Instruction = %b", clk_cntr, instruction_nullifyed);
+      clk_cntr <= clk_cntr + 1;
+    end
+    $finish;
   end
+
 endmodule
