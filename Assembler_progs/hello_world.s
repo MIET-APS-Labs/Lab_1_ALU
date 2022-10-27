ad7eb46123ddae9ce4a9a4d70ad0096ea3160c9d
IlorDash <ilordash@mail.ru>
2022-10-28 01:07:58 +0300

    created my first assembler program

---
+++ Assembler_progs/hello_world.s
@@ -0,0 +1,13 @@
+li x1, 0  # cntr
+li x2, 5  # op 1
+li x3, 8  # op 2
+li x4, 0  # res
+beq x1, x2, 16 # if x1 == x2 then 4
+add x4, x3, x4 # x4 = x3 + x3
+addi x1, x1, 1 # x1 = x1 + 1
+jal x0, -12  # jump to -3 and save position to ra
+nop
+
+
+
+
