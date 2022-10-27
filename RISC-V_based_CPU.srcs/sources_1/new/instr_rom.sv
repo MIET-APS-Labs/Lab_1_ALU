709ec8a675200df14d0ee6129ad27535ff796b70
IlorDash <ilordash@mail.ru>
2022-10-28 00:08:05 +0300

    added nullifying if INIT_FILE is not specified

--- RISC-V_based_CPU.srcs/sources_1/new/instr_rom.sv
+++ RISC-V_based_CPU.srcs/sources_1/new/instr_rom.sv
@@ -2,12 +2,30 @@
 
 module instr_rom #(
     parameter WIDTH = 32,
-    parameter DEPTH = 64
+    parameter DEPTH = 64,
+    parameter INIT_FILE = ""
 ) (
     input [$clog2(DEPTH)-1:0] addr,
     output [WIDTH-1:0] rd
 );
   logic [WIDTH-1:0] ROM[0:DEPTH-1];
-  initial $readmemb("prog.txt", ROM, 0, DEPTH - 1);
+
+  // The following code either initializes the memory values to a specified file or to all zeros to match hardware
+  generate
+    if (INIT_FILE != "") begin : use_init_file
+      initial begin
+        $readmemb(INIT_FILE, ROM, 0, DEPTH - 1);
+      end
+
+    end else begin : init_bram_to_zero
+      integer rom_index;
+      initial begin
+        for (rom_index = 0; rom_index < DEPTH; rom_index = rom_index + 1)
+          ROM[rom_index] = {DEPTH{1'b0}};
+      end
+
+    end
+  endgenerate
+
   assign rd = ROM[addr];
 endmodule
