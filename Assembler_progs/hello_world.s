e65d885af943f9ef997bdf60da300e61851d69ce
IlorDash <ilordash@mail.ru>
2022-11-01 23:11:12 +0300

    added reading result reg x0 at the end of program

--- Assembler_progs/hello_world.s
+++ Assembler_progs/hello_world.s
@@ -6,6 +6,7 @@ beq x1, x2, 16 # if x1 == x2 then 4
 add x4, x3, x4 # x4 = x3 + x3
 addi x1, x1, 1 # x1 = x1 + 1
 jal x0, -12  # jump to -3 and save position to ra
+add x0, x4, x0 # x0 = x4 + x0
 nop
 
 
