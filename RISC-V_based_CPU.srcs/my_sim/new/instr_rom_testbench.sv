9ce44820717c1eda775606ff59ecce5509e20ddc
IlorDash <ilordash@mail.ru>
2022-11-01 23:12:01 +0300

    made byte addressable instruction ROM

--- RISC-V_based_CPU.srcs/my_sim/new/instr_rom_testbench.sv
+++ RISC-V_based_CPU.srcs/my_sim/new/instr_rom_testbench.sv
@@ -1,9 +1,9 @@
 `timescale 1ns / 1ps
 
 `define WORD_LEN 32
+`define BYTE_WIDTH 8
 
-`define INSTR_WIDTH 32
-`define INSTR_DEPTH 64
+`define INSTR_DEPTH 512
 
 module instr_rom_testbench ();
 
@@ -16,35 +16,35 @@ module instr_rom_testbench ();
   end
 
   logic [`WORD_LEN-1:0] instruction_init;
-  instr_rom #(`INSTR_WIDTH, `INSTR_DEPTH, "prog.txt") dut_init (
-      .addr(clk_cntr),
-      .rd  (instruction_init)
+  instr_rom #(`WORD_LEN, `INSTR_DEPTH, "prog.txt") dut_init (
+      .adr(clk_cntr),
+      .rd (instruction_init)
   );
 
   logic [`WORD_LEN-1:0] instruction_nullifyed;
-  instr_rom #(`INSTR_WIDTH, `INSTR_DEPTH) dut_nullifyed (
-      .addr(clk_cntr),
-      .rd  (instruction_nullifyed)
+  instr_rom #(`WORD_LEN, `INSTR_DEPTH) dut_nullifyed (
+      .adr(clk_cntr),
+      .rd (instruction_nullifyed)
   );
 
   parameter COUNTER_WIDTH = $clog2(`INSTR_DEPTH);
   bit [COUNTER_WIDTH-1:0] clk_cntr;
   initial begin
-    $display("\nCheck Instr ROM initialized with mem init file\n");
-    for (int i = 0; i < `INSTR_DEPTH; i++) begin
-      @(posedge CLK);
-      #5;
-      $display("%d) Instruction = %b", clk_cntr, instruction_init);
-      clk_cntr <= clk_cntr + 1;
-    end
-
     $display("\nCheck nullifyed Instr ROM\n");
     clk_cntr <= 0;
-    for (int i = 0; i < `INSTR_DEPTH; i++) begin
+    for (int i = 0; i < `INSTR_DEPTH; i += 4) begin
       @(posedge CLK);
       #5;
       $display("%d) Instruction = %b", clk_cntr, instruction_nullifyed);
-      clk_cntr <= clk_cntr + 1;
+      clk_cntr <= clk_cntr + (`WORD_LEN / `BYTE_WIDTH);
+    end
+    
+    $display("\nCheck Instr ROM initialized with mem init file\n");
+    for (int i = 0; i < `INSTR_DEPTH; i += 4) begin
+      @(posedge CLK);
+      #5;
+      $display("%d) Instruction = %b", clk_cntr, instruction_init);
+      clk_cntr <= clk_cntr + (`WORD_LEN / `BYTE_WIDTH);
     end
     $finish;
   end
--- RISC-V_based_CPU.srcs/sources_1/new/instr_rom.sv
+++ RISC-V_based_CPU.srcs/sources_1/new/instr_rom.sv
@@ -1,31 +1,45 @@
 `timescale 1ns / 1ps
 
+`define BYTE_WIDTH 8
+
 module instr_rom #(
-    parameter WIDTH = 32,
-    parameter DEPTH = 64,
+    parameter WORD_LEN = 32,
+    parameter DEPTH = 512,
     parameter INIT_FILE = ""
 ) (
-    input [$clog2(DEPTH)-1:0] addr,
-    output [WIDTH-1:0] rd
+    input [$clog2(DEPTH)-1:0] adr,
+    output [WORD_LEN-1:0] rd
 );
-  logic [WIDTH-1:0] ROM[0:DEPTH-1];
+
+  // use a 2-dimensional packed array
+  //to model individual bytes
+  logic [`BYTE_WIDTH-1:0] ROM[DEPTH-1:0];
 
   // The following code either initializes the memory values to a specified file or to all zeros to match hardware
   generate
     if (INIT_FILE != "") begin : use_init_file
       initial begin
-        $readmemb(INIT_FILE, ROM, 0, DEPTH - 1);
+        $readmemh(INIT_FILE, ROM);
       end
 
     end else begin : init_bram_to_zero
       integer rom_index;
       initial begin
         for (rom_index = 0; rom_index < DEPTH; rom_index = rom_index + 1)
-          ROM[rom_index] = {DEPTH{1'b0}};
+          ROM[rom_index] = {`BYTE_WIDTH{1'b0}};
       end
 
     end
   endgenerate
 
-  assign rd = ROM[addr];
+  parameter WORD_BYTE_LEN = WORD_LEN / `BYTE_WIDTH;
+  logic [WORD_BYTE_LEN-1:0][`BYTE_WIDTH-1:0] word_o;
+  generate
+    genvar iter_w_o;
+    for (iter_w_o = 0; iter_w_o < WORD_BYTE_LEN; iter_w_o++) begin
+      assign word_o[iter_w_o] = ROM[adr+iter_w_o];
+    end
+  endgenerate
+
+  assign rd = word_o;
 endmodule
