4ec956f000fb97f9001eea728de77d220a1ad193
IlorDash <ilordash@mail.ru>
2022-11-07 17:05:07 +0300

    returned to word addressable Instr rom because of physical constraints of FPGA

--- RISC-V_based_CPU.srcs/my_sim/new/instr_rom_testbench.sv
+++ RISC-V_based_CPU.srcs/my_sim/new/instr_rom_testbench.sv
@@ -1,9 +1,8 @@
 `timescale 1ns / 1ps
 
 `define WORD_LEN 32
-`define BYTE_WIDTH 8
-
-`define INSTR_DEPTH 512
+`define NEXT_INSTR_INCREASE 4
+`define INSTR_DEPTH 64
 
 module instr_rom_testbench ();
 
@@ -22,29 +21,30 @@ module instr_rom_testbench ();
   );
 
   logic [`WORD_LEN-1:0] instruction_nullifyed;
-  instr_rom #(`WORD_LEN, `INSTR_DEPTH) dut_nullifyed (
+  instr_rom dut_nullifyed (
       .adr(clk_cntr),
       .rd (instruction_nullifyed)
   );
 
   parameter COUNTER_WIDTH = $clog2(`INSTR_DEPTH);
-  bit [COUNTER_WIDTH-1:0] clk_cntr;
+  logic [`WORD_LEN-1:0] clk_cntr;
   initial begin
     $display("\nCheck nullifyed Instr ROM\n");
     clk_cntr <= 0;
-    for (int i = 0; i < `INSTR_DEPTH; i += 4) begin
+    for (int i = 0; i < `INSTR_DEPTH; i += `NEXT_INSTR_INCREASE) begin
       @(posedge CLK);
       #5;
       $display("%d) Instruction = %b", clk_cntr, instruction_nullifyed);
-      clk_cntr <= clk_cntr + (`WORD_LEN / `BYTE_WIDTH);
+      clk_cntr <= clk_cntr + `NEXT_INSTR_INCREASE;
     end
-    
+
+    clk_cntr <= 0;
     $display("\nCheck Instr ROM initialized with mem init file\n");
-    for (int i = 0; i < `INSTR_DEPTH; i += 4) begin
+    for (int i = 0; i < `INSTR_DEPTH; i += `NEXT_INSTR_INCREASE) begin
       @(posedge CLK);
       #5;
       $display("%d) Instruction = %b", clk_cntr, instruction_init);
-      clk_cntr <= clk_cntr + (`WORD_LEN / `BYTE_WIDTH);
+      clk_cntr <= clk_cntr + `NEXT_INSTR_INCREASE;
     end
     $finish;
   end
--- RISC-V_based_CPU.srcs/sources_1/new/instr_rom.sv
+++ RISC-V_based_CPU.srcs/sources_1/new/instr_rom.sv
@@ -1,19 +1,22 @@
 `timescale 1ns / 1ps
 
 `define BYTE_WIDTH 8
+`define CORRECTED_ADR_START 2
 
 module instr_rom #(
     parameter WORD_LEN = 32,
-    parameter DEPTH = 512,
+    parameter DEPTH = 64,
     parameter INIT_FILE = ""
 ) (
-    input [$clog2(DEPTH)-1:0] adr,
+    input  [WORD_LEN-1:0] adr,
     output [WORD_LEN-1:0] rd
 );
 
-  // use a 2-dimensional packed array
-  //to model individual bytes
-  logic [`BYTE_WIDTH-1:0] ROM[DEPTH-1:0];
+  logic [WORD_LEN-1:0] ROM[0:DEPTH-1];
+
+  parameter CORRECTED_ADR_LEN = $clog2(DEPTH);
+  logic [`WORD_LEN-1:0] shifted_adr;
+  assign shifted_adr = adr >> `CORRECTED_ADR_START;
 
   // The following code either initializes the memory values to a specified file or to all zeros to match hardware
   generate
@@ -26,20 +29,11 @@ module instr_rom #(
       integer rom_index;
       initial begin
         for (rom_index = 0; rom_index < DEPTH; rom_index = rom_index + 1)
-          ROM[rom_index] = {`BYTE_WIDTH{1'b0}};
+          ROM[rom_index] = {`WORD_LEN{1'b0}};
       end
 
     end
   endgenerate
 
-  parameter WORD_BYTE_LEN = WORD_LEN / `BYTE_WIDTH;
-  logic [0:WORD_BYTE_LEN-1][`BYTE_WIDTH-1:0] word_o;
-  generate
-    genvar iter_w_o;
-    for (iter_w_o = 0; iter_w_o < WORD_BYTE_LEN; iter_w_o++) begin
-      assign word_o[iter_w_o] = ROM[adr+iter_w_o];
-    end
-  endgenerate
-
-  assign rd = word_o;
+  assign rd = ROM[shifted_adr[CORRECTED_ADR_LEN-1:0]];
 endmodule
