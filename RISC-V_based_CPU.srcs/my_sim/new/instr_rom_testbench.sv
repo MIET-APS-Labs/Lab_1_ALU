34abe338d43507ca7918a7b4ef3d2a34498ea58b
IlorDash <ilordash@mail.ru>
2022-11-07 18:15:16 +0300

    added check of read values from ROM

--- RISC-V_based_CPU.srcs/my_sim/new/instr_rom_testbench.sv
+++ RISC-V_based_CPU.srcs/my_sim/new/instr_rom_testbench.sv
@@ -4,6 +4,8 @@
 `define NEXT_INSTR_INCREASE 4
 `define INSTR_DEPTH 64
 
+`define PROG_FILE_NAME "prog.txt"
+
 module instr_rom_testbench ();
 
   logic CLK;
@@ -15,7 +17,7 @@ module instr_rom_testbench ();
   end
 
   logic [`WORD_LEN-1:0] instruction_init;
-  instr_rom #(`WORD_LEN, `INSTR_DEPTH, "prog.txt") dut_init (
+  instr_rom #(`WORD_LEN, `INSTR_DEPTH, `PROG_FILE_NAME) dut_init (
       .adr(clk_cntr),
       .rd (instruction_init)
   );
@@ -26,26 +28,47 @@ module instr_rom_testbench ();
       .rd (instruction_nullifyed)
   );
 
+  int                   fd;  // save prog file descriptot
+  logic [`WORD_LEN-1:0] new_data;  // store new value from file
+
   parameter COUNTER_WIDTH = $clog2(`INSTR_DEPTH);
   logic [`WORD_LEN-1:0] clk_cntr;
   initial begin
     $display("\nCheck nullifyed Instr ROM\n");
     clk_cntr <= 0;
     for (int i = 0; i < `INSTR_DEPTH; i += `NEXT_INSTR_INCREASE) begin
-      @(posedge CLK);
+      @(negedge CLK);
       #5;
-      $display("%d) Instruction = %b", clk_cntr, instruction_nullifyed);
+      if (instruction_nullifyed == 0) begin
+        $display("%d) SUCCESS:  Instruction = %h", clk_cntr, instruction_nullifyed);
+      end else begin
+        $display("%d) FAILED:  Instruction = %h", clk_cntr, instruction_nullifyed);
+      end
       clk_cntr <= clk_cntr + `NEXT_INSTR_INCREASE;
+    end
+
+    fd = $fopen(`PROG_FILE_NAME, "r");
+    if (fd) begin
+      $display("\nFile %s opened SUCCESSFULLY", `PROG_FILE_NAME);
+    end else begin
+      $display("\nFile %s NOT opened", `PROG_FILE_NAME);
     end
 
     clk_cntr <= 0;
     $display("\nCheck Instr ROM initialized with mem init file\n");
     for (int i = 0; i < `INSTR_DEPTH; i += `NEXT_INSTR_INCREASE) begin
-      @(posedge CLK);
+      $fscanf(fd, "%h", new_data);
+      @(negedge CLK);
       #5;
-      $display("%d) Instruction = %b", clk_cntr, instruction_init);
+      if (instruction_init == new_data) begin
+        $display("%d) SUCCESS:  Instruction = %h", clk_cntr, instruction_init);
+      end else begin
+        $display("%d) FAILED:  Instruction = %h, File data = %h", clk_cntr, instruction_init,
+                 new_data);
+      end
       clk_cntr <= clk_cntr + `NEXT_INSTR_INCREASE;
     end
+    $fclose(fd);
     $finish;
   end
 
