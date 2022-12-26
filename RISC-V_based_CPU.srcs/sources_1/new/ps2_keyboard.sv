c333a27d011f0fc7fee130cdd2a89644482fe438
IlorDash <ilordash@mail.ru>
2022-12-26 23:14:04 +0300

    added clk_200 for controlling valid_data_rst_i, added clk_50 generator

--- RISC-V_based_CPU.srcs/sources_1/new/ps2_keyboard.sv
+++ RISC-V_based_CPU.srcs/sources_1/new/ps2_keyboard.sv
@@ -3,7 +3,7 @@
 
 module ps2_keyboard (
     input a_rst_i,
-    input clk_50_i,
+    input clk_200_i,
     input ps2_clk_i,
     input ps2_data_i,
     input valid_data_rst_i,
@@ -12,10 +12,26 @@ module ps2_keyboard (
     output [7:0] data_o
 );
 
+  // CLK divider 200 MHz -> 50 MHz
+  logic clk_50;
+  logic [2:0] clk_cntr_4;
+  parameter DIVISOR = 3'd4;
+  assign clk_50 = clk_cntr_4[2];
+  always_ff @(posedge clk_200_i) begin
+    if (a_rst_i) begin
+      clk_cntr_4 <= 2'b0;
+    end else begin
+      if (clk_cntr_4 >= DIVISOR) begin
+        clk_cntr_4 <= 2'b1;
+      end else begin
+        clk_cntr_4 <= clk_cntr_4 + 1;
+      end
+    end
+  end
 
   // Detect poor ps2_clk_i negedge with register
   logic [9:0] ps2_clk_detect;
-  always_ff @(posedge clk_50_i, posedge a_rst_i) begin
+  always_ff @(posedge clk_50, posedge a_rst_i) begin
     if (a_rst_i) begin
       ps2_clk_detect <= 10'd0;
     end else begin
@@ -59,7 +75,7 @@ module ps2_keyboard (
   // Receiving data
   logic [8:0] shift_reg;
   assign data_o = shift_reg[7:0];
-  always @(posedge clk_50_i, posedge a_rst_i) begin
+  always @(posedge clk_50, posedge a_rst_i) begin
     if (a_rst_i) begin
       shift_reg <= 9'b0;
     end else if (ps2_clk_negedge && ((state == RECEIVE_DATA))) begin
@@ -70,7 +86,7 @@ module ps2_keyboard (
 
   // Counting received bits
   logic [3:0] count_bit;
-  always @(posedge clk_50_i, posedge a_rst_i) begin
+  always @(posedge clk_50, posedge a_rst_i) begin
     if (a_rst_i) begin
       count_bit <= 4'b0;
     end else if (ps2_clk_negedge) begin
@@ -88,7 +104,7 @@ module ps2_keyboard (
     input [7:0] a;
     parity_calc = ~(a[0] ^ a[1] ^ a[2] ^ a[3] ^ a[4] ^ a[5] ^ a[6] ^ a[7]);
   endfunction
-  always @(posedge clk_50_i, posedge a_rst_i) begin
+  always @(posedge clk_200_i, posedge a_rst_i) begin
     if (a_rst_i || valid_data_rst_i) begin
       valid_data_o <= 1'b0;
     end else if (ps2_clk_negedge) begin
@@ -97,7 +113,7 @@ module ps2_keyboard (
           ) == shift_reg[8] && state == CHECK_PARITY_STOP_BITS)
         valid_data_o <= 1'b1;
     end else begin
-      valid_data_o <= 1'b0;
+      //valid_data_o <= 1'b0;
     end
   end
 
